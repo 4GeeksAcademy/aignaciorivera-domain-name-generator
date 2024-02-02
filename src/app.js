07aa43b0395eb37348dc5b8dbaf9e710316658f8
aignaciorivera <152240883+aignaciorivera@users.noreply.github.com>
2024-02-02 21:45:37 +0000

    se agrego los nombres con los respectivos dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".es", ".com"];
+
+  let domainNames = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let a = 0; a < domain.length; a++) {
+          let names = pronoun[i] + adj[j] + noun[k] + domain[a];
+          domainNames.push(names);
+          console.log(domainNames);
+        }
+      }
+    }
+  }
 };
